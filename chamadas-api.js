b7cc6c71e0673b674f4d9730d540b73914899c22
Carla Santos <carla169gama@gmail.com>
2025-11-11 08:53:12 -0300

    cod do zelda

---
+++ chamadas-api.js
@@ -0,0 +1,24 @@
+import { criaCard } from "./card.js";
+import { divResultados } from "./elementos-html.js";
+
+async function buscarCriaturas() {
+  try {
+    const response = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/creatures');
+    const dados = await response.json();
+
+    console.log(dados);
+    
+
+    for (let index = 0; index < dados.data.length; index++) {
+      const card = criaCard(dados.data[index]);
+      divResultados.appendChild(card);
+    }
+    
+  } catch(erro) {
+    console.log('Ocorreu um erro ao buscar os dados na API: ', erro)
+  }
+}
+
+export {
+  buscarCriaturas
+}
---
+++ elementos-html.js
@@ -0,0 +1,16 @@
+const botaoCriaturas = document.getElementById('criaturas');
+const botaoMonstros = document.getElementById('monstros');
+const botaoMateriais = document.getElementById('materiais');
+const botaoEquipamentos = document.getElementById('equipamentos');
+const botaoTesouros = document.getElementById('tesouros');
+
+const divResultados = document.querySelector('.resultados');
+
+export {
+  botaoCriaturas,
+  botaoMonstros,
+  botaoMateriais,
+  botaoEquipamentos,
+  botaoTesouros,
+  divResultados
+}
